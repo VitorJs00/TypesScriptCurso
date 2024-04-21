61bb368017d61faaf9c696844500cfb14cb764c7
VitorJs00 <161177573+VitorJs00@users.noreply.github.com>
2024-04-21 10:01:23 -0300

    feat: insert ts-node

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   semi: true,
   trailingComma: 'all', // Correção: alterado de 'trailngComma' para 'trailingComma'
-  singleQuote: true,
+  singleQuote: false,
   printWidth: 100,
   tabWidth: 2, // Correção: alterado de 'tabidth' para 'tabWidth'
 };
